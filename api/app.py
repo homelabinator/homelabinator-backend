202b0ee36653616aa56d1fd6e3d8488b3af1b87f
MicahBird <31116011+MicahBird@users.noreply.github.com>
2026-03-16 20:48:09 -0600

    Whoops wrong port

--- api/app.py
+++ api/app.py
@@ -192,4 +192,4 @@ async def handle_generate_iso(file: UploadFile = File(...)):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=5001)
+    uvicorn.run(app, host='0.0.0.0', port=5000)
