6e04585675b8a8167bb913e93db831cbe5d929bf
MicahBird <31116011+MicahBird@users.noreply.github.com>
2026-03-19 19:10:02 -0600

    Note to self: the other programs must be bundled in the rust program's path :/

--- nixos-wizard/flake.nix
+++ nixos-wizard/flake.nix
@@ -7,13 +7,18 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, fenix }@inputs:
+  outputs = { self, nixpkgs, fenix, disko }@inputs:
   let
     system = "x86_64-linux";
     mkRustToolchain = fenix.packages.${system}.complete.withComponents;
     pkgs = import nixpkgs { inherit system; };
+    diskoPkg = disko.packages.${system}.disko;
     nixosWizard = pkgs.rustPlatform.buildRustPackage {
       pname = "nixos-wizard";
       version = "0.3.2";
@@ -21,6 +26,23 @@
       src = self;
 
       cargoLock.lockFile = ./Cargo.lock;
+
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+
+      postInstall = ''
+        wrapProgram $out/bin/nixos-wizard \
+        --prefix PATH : ${pkgs.lib.makeBinPath [
+          diskoPkg
+          pkgs.bat
+          pkgs.nixfmt-rfc-style
+          pkgs.nixfmt-classic
+          pkgs.util-linux
+          pkgs.gawk
+          pkgs.gnugrep
+          pkgs.gnused
+          pkgs.ntfs3g
+        ]}
+      '';
     };
   in
   {
--- nixos-wizard/isoimage/flake.nix
+++ nixos-wizard/isoimage/flake.nix
@@ -3,54 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    wizard.url = "path:.."; # Reference the root flake
-    disko = {
-      url = "github:nix-community/disko/latest";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    wizard.url = "path:/home/snowflake/homelabinator-backend/nixos-wizard"; # Reference the root flake
   };
 
-  outputs = { self, nixpkgs, wizard, disko }@inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    diskoPkg = disko.packages.${system}.disko;
-    
-    # Dependencies that were previously in postInstall wrapper of the Rust program
-    wizardDeps = [
-      diskoPkg
-      pkgs.bat
-      pkgs.nixfmt-rfc-style
-      pkgs.nixfmt-classic
-      pkgs.util-linux
-      pkgs.gawk
-      pkgs.gnugrep
-      pkgs.gnused
-      pkgs.ntfs3g
-    ];
+  outputs = { self, nixpkgs, wizard }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    nixosWizard = wizard.packages.${system}.default;
-  in
-  {
-    nixosConfigurations = {
-      installerIso = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit inputs;
-          nixosWizard = nixosWizard;
+      nixosWizard = wizard.packages.${system}.default;
+    in
+    {
+      nixosConfigurations = {
+        installerIso = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            nixosWizard = nixosWizard;
+          };
+          modules = [
+            ./config.nix
+          ];
         };
-        modules = [
-          ./config.nix
-          ({ pkgs, ... }: {
-            environment.systemPackages = wizardDeps;
-          })
-        ];
+      };
+
+      packages.${system} = {
+        default = self.nixosConfigurations.installerIso.config.system.build.isoImage;
+        iso = self.nixosConfigurations.installerIso.config.system.build.isoImage;
       };
     };
-
-    packages.${system} = {
-      default = self.nixosConfigurations.installerIso.config.system.build.isoImage;
-      iso = self.nixosConfigurations.installerIso.config.system.build.isoImage;
-    };
-  };
 }
