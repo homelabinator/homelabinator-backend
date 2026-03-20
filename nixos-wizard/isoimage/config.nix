0b02cf92eb585e90c3bd72265c1b6a0dd44ee058
MicahBird <31116011+MicahBird@users.noreply.github.com>
2026-03-19 19:16:06 -0600

    Correct template path and fix ASCII art

--- nixos-wizard/isoimage/config.nix
+++ nixos-wizard/isoimage/config.nix
@@ -4,23 +4,25 @@
   ];
 
   environment.etc."issue".text = ''
-    _   _                      _       _     _             _             
-    | | | | ___  _ __ ___   ___| | __ _| |__ (_)_ __   __ _| |_ ___  _ __ 
-    | |_| |/ _ \| '_ ` _ \ / _ \ |/ _` | '_ \| | '_ \ / _` | __/ _ \| '__|
-    |  _  | (_) | | | | | |  __/ | (_| | |_) | | | | | (_| | || (_) | |   
-    |_| |_|\___/|_| |_| |_|\___|_|\__,_|_.__/|_|_| |_|\__,_|\__\___/|_|   
+     _    _                      _       _     _             _             
+    | |  | |                    | |     | |   (_)           | |            
+    | |__| | ___  _ __ ___   ___| | __ _| |__  _ _ __   __ _| |_ ___  _ __ 
+    |  __  |/ _ \| '_ ` _ \ / _ \ |/ _` | '_ \| | '_ \ / _` | __/ _ \| '__|
+    | |  | | (_) | | | | | |  __/ | (_| | |_) | | | | | (_| | || (_) | |   
+    |_|  |_|\___/|_| |_| |_|\___|_|\__,_|_.__/|_|_| |_|\__,_|\__\___/|_|   
+                                                                          
                                                                           
 
-    To set up a wireless connection, run `\e[1;35mnmtui\e[0m`.
+     To set up a wireless connection, run `\e[1;35mnmtui\e[0m`.
 
-    Run `\e[1;35msudo nixos-wizard\e[0m` to enter the installer.
+     Run `\e[1;35msudo nixos-wizard\e[0m` to enter the installer.
   '';
 
   # isoImage.squashfsCompression = "gzip -Xcompression-level 1";
   isoImage.squashfsCompression = "zstd -Xcompression-level 1";
   isoImage.contents = [
     {
-      source = ./homelabinator-init-script-template.nix;
+      source = ./homelabinator-init-script.nix;
       target = "/homelabinator-init-script.nix"; # Path on the generated ISO
     }
   ];
